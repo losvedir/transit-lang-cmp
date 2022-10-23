140d693b62522c2ae839a3a944054ba04d2d3e37
Gabe Durazo <gabe@durazo.us>
2022-10-23 16:13:51 -0500

    fix: Elixir, use Jsonrs instead of Jason

--- trexit/config/config.exs
+++ trexit/config/config.exs
@@ -43,7 +43,7 @@ config :logger, :console,
   metadata: [:request_id]
 
 # Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
+config :phoenix, :json_library, Jsonrs
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- trexit/mix.exs
+++ trexit/mix.exs
@@ -46,7 +46,8 @@ defmodule Trexit.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:nimble_csv, "~> 1.2"}
+      {:nimble_csv, "~> 1.2"},
+      {:jsonrs, "~> 0.2"}
     ]
   end
 
