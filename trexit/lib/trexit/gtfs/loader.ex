af165327c74acc68eeb49223abf2d58d96419d8f
Marten/Qqwy <w-m@resilia.nl>
2022-10-24 15:19:40 +0200

    Use :persistent_term instead of :ets.
    Also, use `for` instead of `Enum.map` when iterating over the results.

--- trexit/lib/trexit/gtfs/loader.ex
+++ trexit/lib/trexit/gtfs/loader.ex
@@ -22,24 +22,19 @@ defmodule Trexit.GTFS.Loader do
   end
 
   def load() do
-    :ets.new(:stop_times, [:named_table, {:read_concurrency, true}])
-    :ets.new(:stop_times_ix_by_trip, [:named_table, {:read_concurrency, true}])
-    :ets.new(:trips, [:named_table, {:read_concurrency, true}])
-    :ets.new(:trips_ix_by_route, [:named_table, {:read_concurrency, true}])
-
     {time, _} =
       :timer.tc(fn ->
         get_stop_times()
       end)
 
-    Logger.info("Parsed stop_times.txt in #{time / 1000} ms")
+    Logger.warning("Parsed stop_times.txt in #{time / 1000} ms")
 
     {time, _} =
       :timer.tc(fn ->
         get_trips()
       end)
 
-    Logger.info("Parsed trips.txt in #{time / 1000} ms")
+    Logger.warning("Parsed trips.txt in #{time / 1000} ms")
   end
 
   defp get_stop_times() do
@@ -51,23 +46,17 @@ defmodule Trexit.GTFS.Loader do
     # assert column order
     ["trip_id", "arrival_time", "departure_time", "stop_id" | _] = header
 
-    Enum.with_index(rest, fn [trip_id, arrival_time, departure_time, stop_id | _], i ->
-      case :ets.lookup(:stop_times_ix_by_trip, trip_id) do
-        [] -> :ets.insert(:stop_times_ix_by_trip, {trip_id, [i]})
-        [{_, sts}] -> :ets.insert(:stop_times_ix_by_trip, {trip_id, [i | sts]})
-      end
-
-      :ets.insert(
-        :stop_times,
-        {i,
-         %StopTime{
-           trip_id: trip_id,
-           stop_id: stop_id,
-           arrival: arrival_time,
-           departure: departure_time
-         }}
-      )
+    rest
+    |> Enum.map(fn [trip_id, arrival_time, departure_time, stop_id | _] ->
+      %StopTime{
+        trip_id: trip_id,
+        stop_id: stop_id,
+        arrival: arrival_time,
+        departure: departure_time
+      }
     end)
+    |> Enum.group_by(& &1.trip_id)
+    |> then(&:persistent_term.put({Trexit.GTFS, :stop_times_by_trip}, &1))
   end
 
   defp get_trips() do
@@ -79,21 +68,15 @@ defmodule Trexit.GTFS.Loader do
     # assert column order
     ["route_id", "service_id", "trip_id" | _] = header
 
-    Enum.with_index(rest, fn [route_id, service_id, trip_id | _], i ->
-      case :ets.lookup(:trips_ix_by_route, route_id) do
-        [] -> :ets.insert(:trips_ix_by_route, {route_id, [i]})
-        [{_, trips}] -> :ets.insert(:trips_ix_by_route, {route_id, [i | trips]})
-      end
-
-      :ets.insert(
-        :trips,
-        {i,
-         %Trip{
-           trip_id: trip_id,
-           route_id: route_id,
-           service_id: service_id
-         }}
-      )
+    rest
+    |> Enum.map(fn [route_id, service_id, trip_id | _] ->
+      %Trip{
+        trip_id: trip_id,
+        route_id: route_id,
+        service_id: service_id
+      }
     end)
+    |> Enum.group_by(& &1.route_id)
+    |> then(&:persistent_term.put({Trexit.GTFS, :trips_by_route}, &1))
   end
 end
